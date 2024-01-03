7bd680aa0a090c7a12e9038f4f17a16853ac2684
lemur-wagner <lemur-wagner@users.noreply.github.com>
2024-01-03 19:31:35 +0100

    Added weather API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", displayCity);
+
 function displayCity(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", displayCity);
+function searchCity(city) {
+  apiKey = "fc033e4428deacf92tb6o5f960d83508";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city");
+
+  city.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
